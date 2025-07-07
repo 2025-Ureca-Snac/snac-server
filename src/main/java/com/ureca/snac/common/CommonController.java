478691c1dbd3091d30665542f6173f02d9dc2c1e
JaeYun <116072376+iju42829@users.noreply.github.com>
2025-07-07 16:34:00 +0900

    [chore] CI/CD 초기 세팅 (#6)
    * chore: spring Boot datasource 및 JPA 설정 추가
    
    * chore: CI/CD 배포 워크플로우 추가
    
    * feat: Health Check API 추가

---
+++ src/main/java/com/ureca/snac/common/CommonController.java
@@ -0,0 +1,17 @@
+package com.ureca.snac.common;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.ureca.snac.common.BaseCode.*;
+
+@RestController
+public class CommonController {
+
+    @GetMapping("/health")
+    public ResponseEntity<ApiResponse<?>> health() {
+        return ResponseEntity
+                .ok(ApiResponse.ok(STATUS_OK));
+    }
+}
