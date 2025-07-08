538c5edc261919dbf210acbc19ab52c6b12e32d8
iju42829 <ijud4282@gmail.com>
2025-07-08 14:55:24 +0900

    feat: bank 엔티티 추가

---
+++ src/main/java/com/ureca/snac/finance/entity/Bank.java
@@ -0,0 +1,22 @@
+package com.ureca.snac.finance.entity;
+
+import com.ureca.snac.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "bank")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bank extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "bank_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
