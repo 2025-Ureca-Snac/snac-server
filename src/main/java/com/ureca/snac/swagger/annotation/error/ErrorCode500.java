a616b7c13d8fe10b889c0dc866dac7d0b3bfeee1
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-08 16:48:14 +0900

    feat: add response swagger annotation

---
+++ src/main/java/com/ureca/snac/swagger/annotation/error/ErrorCode500.java
@@ -0,0 +1,22 @@
+package com.ureca.snac.swagger.annotation.error;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.http.ProblemDetail;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+@Documented
+@Target(METHOD)
+@Retention(RUNTIME)
+@ApiResponse(responseCode = "500", description = "서버 오류",
+        content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
+public @interface ErrorCode500 {
+    @AliasFor(annotation = ApiResponse.class, attribute = "description")
+    String description() default "서버 오류";
+}
