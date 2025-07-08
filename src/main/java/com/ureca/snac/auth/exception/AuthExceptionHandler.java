be0e95f7db82bf31e591a948c382a8aba3052200
mike7643 <developerJYumin@gmail.com>
2025-07-08 16:30:11 +0900

    feat: yml mysql로 원상복구

---
+++ src/main/java/com/ureca/snac/auth/exception/AuthExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.ureca.snac.auth.exception;
+
+import com.ureca.snac.common.ApiResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.ureca.snac.auth")
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ApiResponse<Void> handleBusinessException(BusinessException e) {
+        return ApiResponse.of(e.getBaseCode(),null);
+    }
+}
--- src/main/java/com/ureca/snac/common/BaseCode.java
+++ src/main/java/com/ureca/snac/common/BaseCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum BaseCode {
-    STATUS_OK("STATUS_OK_200", HttpStatus.OK, "서버가 정상적으로 동작 중입니다.");
+    // join
+    STATUS_OK("STATUS_OK_200", HttpStatus.OK, "서버가 정상적으로 동작 중입니다."),
+    USER_SIGNUP_SUCCESS("USER_SIGNUP_SUCCESS_201", HttpStatus.CREATED, "정상적으로 회원가입 되었습니다."),
+    EMAIL_DUPLICATE("EMAIL_DUPLICATE_409", HttpStatus.CONFLICT, "이미 사용중인 이메일입니다.");
+
 
     private final String code;
     private final HttpStatus status;
