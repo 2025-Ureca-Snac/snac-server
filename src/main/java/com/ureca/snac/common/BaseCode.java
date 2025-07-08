0531f763803f7e78539fbb6cff3f418f02cc8147
Yumin Jung <121170730+mike7643@users.noreply.github.com>
2025-07-08 16:55:36 +0900

    Update src/main/java/com/ureca/snac/common/BaseCode.java
    Co-authored-by: JaeYun <116072376+iju42829@users.noreply.github.com>

--- src/main/java/com/ureca/snac/common/BaseCode.java
+++ src/main/java/com/ureca/snac/common/BaseCode.java
@@ -11,7 +11,7 @@ public enum BaseCode {
     // join
     STATUS_OK("STATUS_OK_200", HttpStatus.OK, "서버가 정상적으로 동작 중입니다."),
     USER_SIGNUP_SUCCESS("USER_SIGNUP_SUCCESS_201", HttpStatus.CREATED, "정상적으로 회원가입 되었습니다."),
-    EMAIL_DUPLICATE("EMAIL_DUPLICATE_409", HttpStatus.CONFLICT, "이미 사용중인 이메일입니다.");
+    EMAIL_DUPLICATE("EMAIL_DUPLICATE_409", HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."), 
 
     STATUS_OK("STATUS_OK_200", HttpStatus.OK, "서버가 정상적으로 동작 중입니다."),
 
