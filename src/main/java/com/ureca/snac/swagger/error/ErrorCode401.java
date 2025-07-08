d4d58db56ae57738a5f2bc55b4467e820291a613
Yumin Jung <121170730+mike7643@users.noreply.github.com>
2025-07-08 17:17:30 +0900

    fix : 권한 전체로 설정 (#16)
    * fix : 권한 전체로 설정
    
    * refactor: restructure package hierarchy for Swagger annotations

--- src/main/java/com/ureca/snac/swagger/error/ErrorCode401.java
+++ src/main/java/com/ureca/snac/swagger/error/ErrorCode401.java
@@ -1,4 +1,4 @@
-package com.ureca.snac.swagger.annotation.error;
+package com.ureca.snac.swagger.error;
 
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,9 +14,9 @@ import static java.lang.annotation.RetentionPolicy.*;
 @Documented
 @Target(METHOD)
 @Retention(RUNTIME)
-@ApiResponse(responseCode = "400", description = "클라이언트 입력 오류",
+@ApiResponse(responseCode = "401", description = "인증 실패",
         content = @Content(schema = @Schema(implementation = ProblemDetail.class)))
-public @interface ErrorCode400 {
+public @interface ErrorCode401 {
     @AliasFor(annotation = ApiResponse.class, attribute = "description")
-    String description() default "클라이언트 입력 오류";
+    String description() default "인증 실패";
 }
