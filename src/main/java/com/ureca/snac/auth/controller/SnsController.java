fd6aed660a1e159bbb884d62f24c69d540bac80d
mike7643 <developerJYumin@gmail.com>
2025-07-09 23:43:31 +0900

    refactor: SnsService 변경점 분리 및 구현 클래스 SnsServiceImpl 생성

--- src/main/java/com/ureca/snac/auth/controller/SnsController.java
+++ src/main/java/com/ureca/snac/auth/controller/SnsController.java
@@ -2,7 +2,7 @@ package com.ureca.snac.auth.controller;
 
 import com.ureca.snac.auth.dto.request.PhoneRequest;
 import com.ureca.snac.auth.dto.response.VerificationCodeResponse;
-import com.ureca.snac.auth.service.SnsService;
+import com.ureca.snac.auth.service.SnsServiceImpl;
 import com.ureca.snac.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +16,11 @@ import static com.ureca.snac.common.BaseCode.*;
 @RequiredArgsConstructor
 public class SnsController {
 
-    private final SnsService snsService;
+    private final SnsServiceImpl snsServiceImpl;
 
     @PostMapping("/api/send-verification-code")
     public ResponseEntity<ApiResponse<VerificationCodeResponse>> sendVerificationCode(@RequestBody PhoneRequest dto) {
-        String code = snsService.sendVerificationCode(dto.getPhone());
+        String code = snsServiceImpl.sendVerificationCode(dto.getPhone());
         VerificationCodeResponse response = new VerificationCodeResponse(code);
         return ResponseEntity.ok(ApiResponse.of(SMS_VERIFICATION_SENT, response));
     }
