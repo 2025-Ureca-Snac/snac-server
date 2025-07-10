9508a055e8b3fe3df602d56891533ea35bac5998
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-11 02:02:45 +0900

    fix: Wallet Entity 단일 엔티티 모델 채택 ORM의 걸맞게 식별관계로 종속적인거보다 비식별로 PK, FK 관리

--- src/main/java/com/ureca/snac/wallet/entity/Wallet.java
+++ src/main/java/com/ureca/snac/wallet/entity/Wallet.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "wallet")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Wallet extends BaseTimeEntity {
