a3dac3ac2ee10f4a5b2cc9acf726a53c8d8fb844
mike7643 <developerJYumin@gmail.com>
2025-07-14 13:48:23 +0900

    fix : 개발 환경을 위한 JWT 액세스 토큰 만료 시간 조정

--- src/main/java/com/ureca/snac/auth/filter/LoginFilter.java
+++ src/main/java/com/ureca/snac/auth/filter/LoginFilter.java
@@ -69,7 +69,12 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String role = auth.getAuthority();
 
         //토큰 생성
-        String access = jwtUtil.createJwt("access", username, role, 600000L);
+
+        //배포용 access
+        // String access = jwtUtil.createJwt("access", username, role, 600000L);
+
+        // 개발용 access
+        String access = jwtUtil.createJwt("access", username, role, 43200000L);
         String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
         refreshRepository.save(new Refresh(username, refresh));
