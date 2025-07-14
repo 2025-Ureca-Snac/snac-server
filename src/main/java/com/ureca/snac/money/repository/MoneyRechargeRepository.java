558d696ee708b3663226fb7b5714330df467d0e0
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-15 01:55:49 +0900

    refactor: MoneyRecharge, Payment Repository 생성 조회 분리 및 비관적 락 적용

--- src/main/java/com/ureca/snac/money/repository/MoneyRechargeRepository.java
+++ src/main/java/com/ureca/snac/money/repository/MoneyRechargeRepository.java
@@ -1,11 +1,33 @@
 package com.ureca.snac.money.repository;
 
 import com.ureca.snac.money.entity.MoneyRecharge;
+import com.ureca.snac.payment.entity.Payment;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MoneyRechargeRepository extends JpaRepository<MoneyRecharge, Long> {
-    Optional<MoneyRecharge> findByPgOrderId(String pgOrderId);
-    // 토스가 보내주는 주문번호를 통해 우리 시스템에 저장된 충전 요청 기록을 찾는다.
+
+    /**
+     * Payment 객체 MoneyRecharge 조회
+     *
+     * @param payment Payment 객체
+     * @return MoneyRecharge 객체
+     */
+    Optional<MoneyRecharge> findByPayment(Payment payment);
+
+    /**
+     * 비관적 락 사용 결제 취소랑 고려했을 때 데이터 정합성 보장
+     *
+     * @param rechargeId 락걸어 충전 내역 ID
+     * @return 락 걸린 상태 MoneyRecharge 객체
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select mr from MoneyRecharge mr where mr.id= :rechargeId")
+    Optional<MoneyRecharge> findByIdWithLock(@Param("rechargeId") Long rechargeId);
+
 }
