4b8c93b233c1c620096b9eac9a3e78ff5c9da61f
DongHyoen Jeong <144317474+Iamcalmdown@users.noreply.github.com>
2025-07-14 17:55:30 +0900

    hotfix: javadoc TossPayments, Config (#46)
    fix: javadoc TossPayments

--- src/main/java/com/ureca/snac/payments/TossPaymentsClient.java
+++ src/main/java/com/ureca/snac/payments/TossPaymentsClient.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 /**
  * PaymentConfig가 연결해준 API 통신 전담 클래스
+ * RestClient, Properties 를 주입 받고 API 명세
+ * 에러 처리, 설정 관리, 책임 분리
  */
 
 @RequiredArgsConstructor
@@ -16,6 +18,14 @@ public class TossPaymentsClient {
     private final RestClient restClient;
     private final TossPaymentProperties properties;
 
+    /**
+     * 결제  승인 API 를 호출하여 최종적으로 결제 완료
+     *
+     * @param paymentKey 토스페이먼츠가 발근하는 키
+     * @param orderId    우리 시스템의 주문번호
+     * @param amount     결제 금액
+     * @return 결제 승인 후 토스페이먼츠로 받은 응답 정보를 담은 TossConfirmResponse 객체
+     */
     public TossConfirmResponse confirmPayment(String paymentKey, String orderId, Long amount) {
 
         return restClient.post()
