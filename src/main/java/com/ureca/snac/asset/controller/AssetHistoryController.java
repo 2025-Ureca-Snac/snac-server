0d9709d2ad518ecc7cace59d1660d09c20319d38
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-19 15:31:30 +0900

    feat: AssetHistory 조회 컨트롤러 API 구현

---
+++ src/main/java/com/ureca/snac/asset/controller/AssetHistoryController.java
@@ -0,0 +1,33 @@
+package com.ureca.snac.asset.controller;
+
+import com.ureca.snac.asset.dto.AssetHistoryListRequest;
+import com.ureca.snac.asset.dto.AssetHistoryListResponse;
+import com.ureca.snac.asset.service.AssetHistoryService;
+import com.ureca.snac.auth.dto.CustomUserDetails;
+import com.ureca.snac.common.ApiResponse;
+import com.ureca.snac.swagger.annotation.UserInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.ureca.snac.common.BaseCode.ASSET_HISTORY_SUCCESS;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AssetHistoryController implements AssetHistorySwagger {
+
+    private final AssetHistoryService assetHistoryService;
+
+    @Override
+    public ResponseEntity<ApiResponse<AssetHistoryListResponse>> getMyAssetHistories(
+            AssetHistoryListRequest request, @UserInfo CustomUserDetails userDetails) {
+        log.info("[자산 내역 조회 시작] 회원 : {}, 요청 : {}", userDetails.getUsername(), request);
+
+        AssetHistoryListResponse response = assetHistoryService.getAssetHistories(userDetails.getUsername(), request);
+
+        log.info("[자산 내역 조회 완료] 회원 : {}, 조회된 내역 개수 : {}", userDetails.getUsername(), response.histories().size());
+        return ResponseEntity.ok(ApiResponse.of(ASSET_HISTORY_SUCCESS, response));
+    }
+}
---
+++ src/main/java/com/ureca/snac/asset/controller/AssetHistorySwagger.java
@@ -0,0 +1,40 @@
+package com.ureca.snac.asset.controller;
+
+import com.ureca.snac.asset.dto.AssetHistoryListRequest;
+import com.ureca.snac.asset.dto.AssetHistoryListResponse;
+import com.ureca.snac.auth.dto.CustomUserDetails;
+import com.ureca.snac.common.ApiResponse;
+import com.ureca.snac.swagger.annotation.UserInfo;
+import com.ureca.snac.swagger.annotation.error.ErrorCode400;
+import com.ureca.snac.swagger.annotation.error.ErrorCode401;
+import com.ureca.snac.swagger.annotation.error.ErrorCode403;
+import com.ureca.snac.swagger.annotation.error.ErrorCode500;
+import com.ureca.snac.swagger.annotation.response.ApiSuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "자산 내역 API", description = "사용자의 자산 변동 내역을 관리")
+@RequestMapping("/api/asset-histories")
+public interface AssetHistorySwagger {
+
+    @Operation(summary = "내 자산 내역 목록 조회",
+            description = "인증된 사용자의 자산 내역(스낵 머니 나 포인트) 조회")
+    @SecurityRequirement(name = "Authorization")
+    @ApiSuccessResponse(description = "자산 내역 조회 성공")
+    @ErrorCode400(description = "잘못된 요청 파라미터입니다.")
+    @ErrorCode401
+    @ErrorCode403(description = "권한없음")
+    @ErrorCode500
+    @GetMapping("/me")
+    ResponseEntity<ApiResponse<AssetHistoryListResponse>> getMyAssetHistories(
+            @ParameterObject @Valid @ModelAttribute AssetHistoryListRequest request,
+            @UserInfo CustomUserDetails userDetails
+    );
+}
