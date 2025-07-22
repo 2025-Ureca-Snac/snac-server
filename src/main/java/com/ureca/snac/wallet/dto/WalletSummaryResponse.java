c543e708ff0dd185c1f29eeef8e595e6126b0d88
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-22 14:01:03 +0900

    feat: 지갑 잔액 요약 조회 API 엔드포인트 추가

---
+++ src/main/java/com/ureca/snac/wallet/dto/WalletSummaryResponse.java
@@ -0,0 +1,15 @@
+package com.ureca.snac.wallet.dto;
+
+import com.ureca.snac.wallet.entity.Wallet;
+
+public record WalletSummaryResponse(Long money, Long point) {
+    /**
+     * 엔티티 객체를 받아서 DTO를 반환하는 정적 팩토리 메소드
+     *
+     * @param wallet 반환할 원본 엔티티
+     * @return DTO
+     */
+    public static WalletSummaryResponse from(Wallet wallet) {
+        return new WalletSummaryResponse(wallet.getMoney(), wallet.getPoint());
+    }
+}
