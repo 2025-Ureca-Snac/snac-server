3f1e545915f8e99be19d6c07379d7193e1857052
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-07-31 02:41:04 +0900

    feat: settlementValidator 분리 application 계층 및 domain 계층 서비스 분리

---
+++ src/main/java/com/ureca/snac/settlement/domain/service/SettlementValidator.java
@@ -0,0 +1,24 @@
+package com.ureca.snac.settlement.domain.service;
+
+import com.ureca.snac.finance.entity.Account;
+import com.ureca.snac.finance.exception.AccountNotFoundException;
+import com.ureca.snac.finance.repository.AccountRepository;
+import com.ureca.snac.member.entity.Member;
+import com.ureca.snac.settlement.exception.SettlementAccountMismatchException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SettlementValidator {
+
+    private final AccountRepository accountRepository;
+
+    public void validate(Member member, String inputAccountNumber) {
+        Account registeredAccount = accountRepository.findByMember(member)
+                .orElseThrow(AccountNotFoundException::new);
+
+        if (!registeredAccount.isSameAccountNumber(inputAccountNumber))
+            throw new SettlementAccountMismatchException();
+    }
+}
