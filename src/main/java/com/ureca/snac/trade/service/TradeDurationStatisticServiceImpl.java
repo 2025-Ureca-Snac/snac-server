c078d4541b98cc857f7f8700fc0c559c4a0fffdb
iju42829 <ijud4282@gmail.com>
2025-07-30 21:05:23 +0900

    feat: 거래 소요 시간 통계 스케줄링 및 조회 서비스 구현

--- src/main/java/com/ureca/snac/trade/service/TradeDurationStatisticServiceImpl.java
+++ src/main/java/com/ureca/snac/trade/service/TradeDurationStatisticServiceImpl.java
@@ -7,14 +7,16 @@ import com.ureca.snac.trade.service.interfaces.TradeDurationStatisticService;
 import com.ureca.snac.trade.service.response.TradeDurationStatisticResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
+@Service
 @Transactional
 @RequiredArgsConstructor
 public class TradeDurationStatisticServiceImpl implements TradeDurationStatisticService {
 
-    private TradeDurationStatisticRepository tradeDurationStatisticRepository;
+    private final TradeDurationStatisticRepository tradeDurationStatisticRepository;
 
     @Override
     public TradeDurationStatisticResponse getLatestStatistic() {
