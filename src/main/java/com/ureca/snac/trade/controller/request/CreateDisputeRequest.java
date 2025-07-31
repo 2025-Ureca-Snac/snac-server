37098fe92801972d5450c29a2ab1fd476069bd49
seokjuun <seokjuun@gmail.com>
2025-08-01 05:53:03 +0900

    feat: 실시간 신고 생성 시 자동 거래 취소 및 알림 발송 로직 추가
    - 신고 등록시 자동으로 거래 취소되도록 MatchingServiceFacade 개선
    - 거래 취소 사유에 REAL_TIME_REPORTED 추가
    - 신고 발생 시 MQ 알림 및 거래 취소 알림 동시 발송
    - MatchingController에 신고(ws) 엔드포인트 추가

---
+++ src/main/java/com/ureca/snac/trade/controller/request/CreateDisputeRequest.java
@@ -0,0 +1,20 @@
+package com.ureca.snac.trade.controller.request;
+
+import com.ureca.snac.trade.entity.DisputeType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateDisputeRequest {
+    private Long tradeId;
+    private DisputeType type;
+    private String description;
+    private List<String> attachmentKeys;
+
+
+}
