3de7ea0d32942997a5941efeeda8cedc0200418b
iju42829 <ijud4282@gmail.com>
2025-07-31 20:10:59 +0900

    feat: 거래 취소 시 문자 전송 기능 추가

--- src/main/java/com/ureca/snac/trade/support/TradeMessageBuilder.java
+++ src/main/java/com/ureca/snac/trade/support/TradeMessageBuilder.java
@@ -34,56 +34,57 @@ public class TradeMessageBuilder {
 
         List<String> phoneList = new ArrayList<>();
 
-        if (status == TradeStatus.PAYMENT_CONFIRMED && card.getCardCategory() == CardCategory.BUY && card.getSellStatus() == TRADING) {
-            statusText = "구매글 거래가 시작되었습니다";
+        if (status == TradeStatus.PAYMENT_CONFIRMED_ACCEPTED) {
+            statusText = "[SNAC] 구매글 거래가 시작되었습니다. 판매자와 연락하여 진행해 주세요.";
             phoneList.add(trade.getBuyer().getPhone());
 
         } else {
             switch (status) {
                 case PAYMENT_CONFIRMED:
-                    statusText = "결제가 완료되었습니다";
+                    statusText = "[SNAC] 결제가 완료되었습니다. 판매자는 데이터 전송을 진행해 주세요.";
                     phoneList.add(trade.getSeller().getPhone());
                     break;
 
                 case DATA_SENT:
-                    statusText = "데이터 전송이 완료되었습니다";
+                    statusText = "[SNAC] 데이터가 전송되었습니다. 구매자는 확인 후 확정해 주세요.";
                     phoneList.add(trade.getBuyer().getPhone());
                     break;
 
                 case COMPLETED:
-                    statusText = "거래가 확정되었습니다";
+                    statusText = "[SNAC] 거래가 최종 확정되었습니다. 이용해 주셔서 감사합니다.";
                     phoneList.add(trade.getSeller().getPhone());
                     break;
 
                 case CANCELED:
-                    statusText = "거래가 취소되었습니다";
+                    statusText = "[SNAC] 거래가 취소되었습니다. 사유를 확인해 주세요.";
                     phoneList.add(trade.getSeller().getPhone());
                     phoneList.add(trade.getBuyer().getPhone());
                     break;
 
                 case AUTO_REFUND:
-                    statusText = "판매자 지연으로 자동 환불되었습니다";
+                    statusText = "[SNAC] 판매자 지연으로 인해 자동으로 환불 처리되었습니다.";
                     phoneList.add(trade.getSeller().getPhone());
                     phoneList.add(trade.getBuyer().getPhone());
                     break;
                 case AUTO_PAYOUT:
-                    statusText = "구매자 미확정으로 자동 정산되었습니다";
+                    statusText = "[SNAC] 구매자가 확정하지 않아 자동으로 정산 처리되었습니다.";
                     phoneList.add(trade.getSeller().getPhone());
                     phoneList.add(trade.getBuyer().getPhone());
                     break;
 
                 default:
-                    statusText = "상태가 변경되었습니다";
+                    statusText = "[SNAC] 거래 상태가 변경되었습니다. 앱에서 상세 내용을 확인해 주세요.";
             }
         }
 
         // 데이터량 GB, 가격 원 단위 포맷
         int dataGb = trade.getDataAmount();
-        String priceText = String.format("%,d원", trade.getPriceGb());
+//        String priceText = String.format("%,d원", trade.getPriceGb());
 
-        return new TradeMessageDto(phoneList, String.format(
-                "[SNAC] 거래 ID %d: %s.\n데이터 %dGB, 거래 금액 %s.\n",
-                trade.getId(), statusText, dataGb, priceText
-        ));
+//        return new TradeMessageDto(phoneList, String.format(
+//                "[SNAC] 거래 ID %d: %s.\n데이터 %dGB, 거래 금액 %s.\n",
+//                trade.getId(), statusText, dataGb, priceText
+//        ));
+        return new TradeMessageDto(phoneList, statusText);
     }
 }
