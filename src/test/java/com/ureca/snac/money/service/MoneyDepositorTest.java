183aeb4e0d2b068a8af549f5655d6118f84f4af6
Iamcalmdown <ehsk2315@kyungsung.ac.kr>
2025-08-03 17:49:43 +0900

    feat: 서비스 계층 단위테스트

---
+++ src/test/java/com/ureca/snac/money/service/MoneyDepositorTest.java
@@ -0,0 +1,118 @@
+package com.ureca.snac.money.service;
+
+import com.ureca.snac.asset.event.AssetChangedEvent;
+import com.ureca.snac.asset.service.AssetChangedEventFactory;
+import com.ureca.snac.asset.service.AssetHistoryEventPublisher;
+import com.ureca.snac.infra.dto.response.TossConfirmResponse;
+import com.ureca.snac.member.entity.Member;
+import com.ureca.snac.money.entity.MoneyRecharge;
+import com.ureca.snac.money.repository.MoneyRechargeRepository;
+import com.ureca.snac.payment.entity.Payment;
+import com.ureca.snac.payment.entity.PaymentStatus;
+import com.ureca.snac.payment.repository.PaymentRepository;
+import com.ureca.snac.support.TestFixture;
+import com.ureca.snac.wallet.exception.WalletNotFoundException;
+import com.ureca.snac.wallet.service.WalletService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MoneyDepositorTest {
+
+    @InjectMocks
+    private MoneyDepositor moneyDepositor;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock
+    private MoneyRechargeRepository moneyRechargeRepository;
+
+    @Mock
+    private WalletService walletService;
+
+    @Mock
+    private AssetHistoryEventPublisher assetHistoryEventPublisher;
+
+    @Mock
+    private AssetChangedEventFactory assetChangedEventFactory;
+
+    private Member member;
+    private Payment payment;
+    private TossConfirmResponse tossConfirmResponse;
+
+    @BeforeEach
+    void setUp() {
+        member = TestFixture.createTestMember();
+        payment = TestFixture.createPendingPayment(member);
+        tossConfirmResponse = TestFixture.createTossConfirmResponse();
+
+        // 공통 Mock 객체 정의 영속 상태 객체 반환
+        when(paymentRepository.save(any(Payment.class))).thenAnswer(invocation -> {
+            Payment paymentToSave = invocation.getArgument(0);
+            ReflectionTestUtils.setField(paymentToSave, "id", 1L);
+            return paymentToSave;
+        });
+    }
+
+    @Test
+    void 머니_입금_처리시_정상적_상호작용() {
+        // given
+        // 가짜 충전 내역 저장소에 저장 해서 돌려준다, null 처리
+        when(moneyRechargeRepository.save(any(MoneyRecharge.class))).
+                thenAnswer(invocation -> {
+                    MoneyRecharge recharge = invocation.getArgument(0);
+                    ReflectionTestUtils.setField(recharge, "id", 1L);
+                    return recharge;
+                });
+        // 지갑 서비스 입금 요청
+        when(walletService.depositMoney(member.getId(), 10000L)).
+                thenReturn(20000L);
+        // 이벤트 생성
+        when(assetChangedEventFactory.createForRechargeEvent(anyLong(), anyLong(), anyLong(), anyLong()))
+                .thenReturn(TestFixture.createDummyEvent());
+
+        // when 테스트 시작
+        moneyDepositor.deposit(payment, member, tossConfirmResponse);
+
+        // then
+        // Payment 저장
+        verify(paymentRepository).save(payment);
+
+        // MoneyRecharge 저장
+        verify(moneyRechargeRepository).save(any(MoneyRecharge.class));
+
+        // wallet 증가
+        verify(walletService).depositMoney(member.getId(), 10000L);
+
+        // Asset 이벤트
+        verify(assetChangedEventFactory).createForRechargeEvent(anyLong(), anyLong(), anyLong(), anyLong());
+        verify(assetHistoryEventPublisher).publish(any(AssetChangedEvent.class));
+
+        assertThat(payment.getStatus()).isEqualTo(PaymentStatus.SUCCESS);
+    }
+
+    @Test
+    void 지갑_입금_실패시_충전기록_저장_안됨() {
+        // given
+        when(walletService.depositMoney(member.getId(), 10000L))
+                .thenThrow(new WalletNotFoundException());
+
+        // when then
+        assertThatThrownBy(() -> moneyDepositor.
+                deposit(payment, member, tossConfirmResponse))
+                .isInstanceOf(WalletNotFoundException.class);
+
+        // 트랜잭션 롤백
+        verify(paymentRepository).save(payment);
+    }
+}
--- src/test/java/com/ureca/snac/money/service/MoneyServiceImplTest.java
+++ src/test/java/com/ureca/snac/money/service/MoneyServiceImplTest.java
@@ -1,6 +1,5 @@
 package com.ureca.snac.money.service;
 
-import com.ureca.snac.common.BaseCode;
 import com.ureca.snac.common.exception.InternalServerException;
 import com.ureca.snac.infra.PaymentGatewayAdapter;
 import com.ureca.snac.infra.dto.response.TossConfirmResponse;
@@ -9,6 +8,8 @@ import com.ureca.snac.member.repository.MemberRepository;
 import com.ureca.snac.payment.entity.Payment;
 import com.ureca.snac.payment.repository.PaymentRepository;
 import com.ureca.snac.payment.service.PaymentRecoveryService;
+import com.ureca.snac.support.TestFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,11 +18,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.ureca.snac.common.BaseCode.PAYMENT_INTERNAL_ERROR;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith((MockitoExtension.class))
+@ExtendWith(MockitoExtension.class)
 class MoneyServiceImplTest {
 
     // 테스트할 대상은 구현체
@@ -44,50 +47,72 @@ class MoneyServiceImplTest {
     @Mock
     PaymentRecoveryService paymentRecoveryService;
 
-    // 가짜 객체
-    @Mock
-    private Member mockMember;
+    private Member member;
+    private Payment payment;
+    private TossConfirmResponse tossConfirmResponse;
 
-    @Mock
-    private Payment mockPayment;
+    @BeforeEach
+    void setUp() {
+        member = TestFixture.createTestMember();
+        payment = TestFixture.createPendingPayment(member);
+        tossConfirmResponse = TestFixture.createTossConfirmResponse();
+    }
 
-    @Mock
-    private TossConfirmResponse mockResponse;
+    @Test
+    void 머니_충전_성공_시나리오() {
+        // given
+        when(memberRepository.findByEmail(anyString())).
+                thenReturn(Optional.of(member));
+
+        when(paymentRepository.findByOrderIdWithMember(anyString()))
+                .thenReturn(Optional.of(payment));
+
+        when(paymentGatewayAdapter.confirmPayment(anyString(), anyString(), anyLong()))
+                .thenReturn(tossConfirmResponse);
+
+        // when
+        moneyServiceImpl.processRechargeSuccess(
+                "test_key_id", "test_order_id", 10000L, "test@test.com");
+
+        // then
+        verify(paymentRepository).findByOrderIdWithMember("test_order_id");
+        verify(paymentGatewayAdapter).confirmPayment("test_key_id", "test_order_id", 10000L);
+        verify(moneyDepositor).deposit(payment, member, tossConfirmResponse);
+        // 호출 X
+        verify(paymentRecoveryService, never()).processInternalFailure(any(), any());
+    }
 
     @Test
     void 결제성공_후_내부_DB에서_처리실패_시_복구_로직_호출_및_예외() {
-
-        // 1. 테스트에 필요한 예외 객체
+        // given
         RuntimeException dbException = new RuntimeException("DB 연결 실패");
 
         when(memberRepository.findByEmail(anyString())).
-                thenReturn(Optional.of(mockMember));
+                thenReturn(Optional.of(member));
 
         when((paymentRepository.findByOrderIdWithMember(anyString()))).
-                thenReturn(Optional.of(mockPayment));
+                thenReturn(Optional.of(payment));
 
         when(paymentGatewayAdapter.confirmPayment(anyString(), anyString(), anyLong()))
-                .thenReturn(mockResponse);
+                .thenReturn(tossConfirmResponse);
 
         // deposit 호출 예외발생위해
         doThrow(dbException).when(moneyDepositor)
                 .deposit(any(Payment.class), any(Member.class), any(TossConfirmResponse.class));
 
         // InternalServerException 이 던져지는지 검증
+        // when then
         InternalServerException thrownException = assertThrows(
                 InternalServerException.class,
                 () -> moneyServiceImpl.processRechargeSuccess("test_key_id",
-                        "test_order_id",
-                        10L,
-                        "test@tset.com"
+                        "test_order_id", 10000L, "test@tset.com"
                 )
         );
 
         // 2. 던져진 예외가 우리가 의도한건지 확인
-        assertEquals(BaseCode.PAYMENT_INTERNAL_ERROR, thrownException.getBaseCode());
+        assertThat(thrownException.getBaseCode()).isEqualTo(PAYMENT_INTERNAL_ERROR);
 
-        // 어떤 Payment 객체와 어떤 예외 객체가 전달되었는지 확인
         verify(paymentRecoveryService, times(1))
-                .processInternalFailure(mockPayment, dbException);
+                .processInternalFailure(payment, dbException);
     }
 }
